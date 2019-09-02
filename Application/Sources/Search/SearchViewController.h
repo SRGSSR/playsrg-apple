77e2288f24b93a00a0b77f034e823c7f53d886a2
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2019-09-02 17:43:15 +0200

    Update documentation

--- Application/Sources/Search/SearchViewController.h
+++ Application/Sources/Search/SearchViewController.h
@@ -16,7 +16,7 @@ NS_ASSUME_NONNULL_BEGIN
  *  Create a search view controller with optional query and settings.
  *
  *  @param query     The query.
- *  @param settings  The search settings. Only set if the application can use them (@see `searchSettingsDisabled` in `ApplicationConfiguration`).
+ *  @param settings  The search settings. Only used if search settings is enabled (@see `searchSettingsDisabled` in `ApplicationConfiguration`).
  */
 - (instancetype)initWithQuery:(nullable NSString *)query settings:(nullable SRGMediaSearchSettings *)settings;
 
