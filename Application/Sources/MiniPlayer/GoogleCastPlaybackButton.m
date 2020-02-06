62e059a72943f11d65995c034b8fabb62d197fa5
Samuel Défago <defagos@gmail.com>
2020-02-06 14:53:45 +0100

    Display stop buttons for Google Cast as well

--- Application/Sources/MiniPlayer/GoogleCastPlaybackButton.m
+++ Application/Sources/MiniPlayer/GoogleCastPlaybackButton.m
@@ -14,7 +14,15 @@
 
 - (NSString *)accessibilityLabel
 {
-    return (self.buttonState == GCKUIButtonStatePause) ? PlaySRGAccessibilityLocalizedString(@"Play", @"Play button label") : PlaySRGAccessibilityLocalizedString(@"Pause", @"Pause button label");
+    static dispatch_once_t s_onceToken;
+    static NSDictionary<NSNumber *, NSString *> *s_accessibilityLabels;
+    dispatch_once(&s_onceToken, ^{
+        s_accessibilityLabels = @{ @(GCKUIButtonStatePause) : PlaySRGAccessibilityLocalizedString(@"Play", @"Play button label"),
+                                   @(GCKUIButtonStatePlay) : PlaySRGAccessibilityLocalizedString(@"Pause", @"Play button label"),
+                                   @(GCKUIButtonStatePlayLive) : PlaySRGAccessibilityLocalizedString(@"Stop", @"Play button label") };
+    });
+    
+    return s_accessibilityLabels[@(self.buttonState)];
 }
 
 @end
