0a2a6b6a6afcc2696bf00f208bf8c363a196cf29
Samuel Défago <defagos@gmail.com>
2020-02-11 19:35:20 +0100

    Fix topic media list layout and remove associated storyboard

--- Application/Sources/Home/HomeMediasViewController.m
+++ Application/Sources/Home/HomeMediasViewController.m
@@ -45,15 +45,24 @@
 
 #pragma mark View lifecycle
 
-- (void)viewDidLoad
+- (void)loadView
 {
-    [super viewDidLoad];
+    UIView *view = [[UIView alloc] initWithFrame:UIScreen.mainScreen.bounds];
+    view.backgroundColor = UIColor.play_blackColor;
     
-    self.view.backgroundColor = UIColor.play_blackColor;
+    UICollectionViewFlowLayout *collectionViewLayout = [[UICollectionViewFlowLayout alloc] init];
+    collectionViewLayout.minimumInteritemSpacing = 0.f;
+    collectionViewLayout.minimumLineSpacing = 0.f;
     
-    self.collectionView.backgroundColor = UIColor.clearColor;
-    self.collectionView.indicatorStyle = UIScrollViewIndicatorStyleWhite;
-    self.collectionView.alwaysBounceVertical = YES;
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:view.bounds collectionViewLayout:collectionViewLayout];
+    collectionView.backgroundColor = UIColor.clearColor;
+    collectionView.indicatorStyle = UIScrollViewIndicatorStyleWhite;
+    collectionView.alwaysBounceVertical = YES;
+    collectionView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [view addSubview:collectionView];
+    self.collectionView = collectionView;
+    
+    self.view = view;
 }
 
 #pragma mark Rotation
