0c0688c6877270665e43f2e9d3d1f211a1825806
Samuel Défago <defagos@gmail.com>
2020-02-11 15:52:21 +0100

    Optimize history screen layout

--- Application/Sources/History/HistoryViewController.m
+++ Application/Sources/History/HistoryViewController.m
@@ -17,7 +17,6 @@
 
 #import <libextobjc/libextobjc.h>
 #import <SRGAnalytics/SRGAnalytics.h>
-#import <SRGAppearance/SRGAppearance.h>
 #import <SRGUserData/SRGUserData.h>
 
 @interface HistoryViewController () <HistoryTableViewCellDelegate>
@@ -185,8 +184,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSString *contentSizeCategory = UIApplication.sharedApplication.preferredContentSizeCategory;
-    return (SRGAppearanceCompareContentSizeCategories(contentSizeCategory, UIContentSizeCategoryExtraLarge) == NSOrderedAscending) ? 94.f : 110.f;
+    return 94.f;
 }
 
 - (void)tableView:(UITableView *)tableView willDisplayCell:(HistoryTableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
