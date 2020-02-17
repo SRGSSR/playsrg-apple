fdabeb60ce059a6c9278cdd749dac43c714427bb
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2020-02-17 20:30:11 +0100

    Fix typo

--- Application/Sources/Shows/ShowViewController.h
+++ Application/Sources/Shows/ShowViewController.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ShowViewController : MediasViewController <UIGestureRecognizerDelegate, SRGAnalyticsViewTracking>
+@interface ShowViewController : MediasViewController <SRGAnalyticsViewTracking, UIGestureRecognizerDelegate>
 
 - (instancetype)initWithShow:(SRGShow *)show fromPushNotification:(BOOL)fromPushNotification NS_DESIGNATED_INITIALIZER;
 
