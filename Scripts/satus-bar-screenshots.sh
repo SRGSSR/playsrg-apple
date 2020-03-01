68f89356c40cb352109036941b0fb4baa22196bc
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2020-03-01 17:50:34 +0100

    Clean status bar
    Work with iOS 13 simulators only

---
+++ Scripts/satus-bar-screenshots.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e -x
+
+xcrun simctl boot "${TARGET_DEVICE_IDENTIFIER}"
+
+xcrun simctl status_bar "${TARGET_DEVICE_IDENTIFIER}" override \
+    --time "2020-01-01T09:41:00+0100" \
+    --dataNetwork "wifi" \
+    --wifiMode "active" \
+    --wifiBars "3" \
+    --cellularMode "notSupported" \
+    --batteryState "charged" \
+    --batteryLevel "100"
