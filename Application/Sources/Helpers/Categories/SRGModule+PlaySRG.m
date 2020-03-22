f569449530e822785a626eedd4d589cf5679585c
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2020-03-22 22:44:23 +0100

    Neutralise the web portal background color

--- Application/Sources/Helpers/Categories/SRGModule+PlaySRG.m
+++ Application/Sources/Helpers/Categories/SRGModule+PlaySRG.m
@@ -6,11 +6,22 @@
 
 #import "SRGModule+PlaySRG.h"
 
+#import "UIColor+PlaySRG.h"
+
+#import <SRGAppearance/SRGAppearance.h>
+
 @implementation SRGModule (PlaySRG)
 
 - (UIColor *)play_backgroundColor
 {
-    return [self.backgroundColor colorWithAlphaComponent:.3f];
+    UIColor *backgroundColor = self.backgroundColor;
+    
+    // TODO: Remove when Play apps and web portal will use the same black background color
+    if ([backgroundColor isEqual:[UIColor srg_colorFromHexadecimalString:@"#1A1A1A"]]) {
+        backgroundColor = UIColor.play_blackColor;
+    }
+    
+    return [backgroundColor colorWithAlphaComponent:.3f];
 }
 
 @end
