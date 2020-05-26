03603fcf10b34ccdb5c3720d6cd20c34e7cbc4ec
Samuel Défago <defagos@gmail.com>
2020-05-26 08:57:55 +0200

    Add documentation

--- Application/Sources/Helpers/ChannelServiceSetup.h
+++ Application/Sources/Helpers/ChannelServiceSetup.h
@@ -8,10 +8,19 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ *  Setup common to a group of channel update registrations.
+ */
 @interface ChannelServiceSetup : NSObject <NSCopying>
 
+/**
+ *  Setup for retrieval of updates for a given channel, vendor and livestream identifier.
+ */
 - (instancetype)initWithChannel:(SRGChannel *)channel vendor:(SRGVendor)vendor livestreamUid:(NSString *)livestreamUid;
 
+/**
+ *  Associated data.
+ */
 @property (nonatomic, readonly) SRGChannel *channel;
 @property (nonatomic, readonly) SRGVendor vendor;
 @property (nonatomic, readonly, copy) NSString *livestreamUid;
