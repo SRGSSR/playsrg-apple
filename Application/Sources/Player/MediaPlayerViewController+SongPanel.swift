a4eab435833767aa6982681e873c414db9a6b54f
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2020-06-23 11:57:27 +0200

    Update VoiceOver song list panel labels

--- Application/Sources/Player/MediaPlayerViewController+SongPanel.swift
+++ Application/Sources/Player/MediaPlayerViewController+SongPanel.swift
@@ -262,10 +262,10 @@ extension MediaPlayerViewController : PanelAccessibilityDelegate {
     
     public func panel(_ panel: Panel, accessibilityLabelForResizeHandle resizeHandle: ResizeHandle) -> String {
         if panel.configuration.mode == .compact {
-            return PlaySRGAccessibilityLocalizedString("Hide music", "Accessibility label of the song list handle when closed")
+            return PlaySRGAccessibilityLocalizedString("Show music list", "Accessibility label of the song list handle when closed")
         }
         else {
-            return PlaySRGAccessibilityLocalizedString("Display music", "Accessibility label of the song list handle when open")
+            return PlaySRGAccessibilityLocalizedString("Hide music list", "Accessibility label of the song list handle when opened")
         }
     }
     
