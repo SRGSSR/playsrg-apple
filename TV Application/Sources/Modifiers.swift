ec263dde0e64a3533b67eb302faa82934bfe111e
Samuel Défago <defagos@gmail.com>
2020-09-13 20:31:31 +0200

    Perform action when the network is reachable again

--- TV Application/Sources/Modifiers.swift
+++ TV Application/Sources/Modifiers.swift
@@ -4,12 +4,18 @@
 //  License information is available from the LICENSE file.
 //
 
+import FXReachability
 import SwiftUI
 
 extension View {
     func onResume(perform action: @escaping () -> Void) -> some View {
         onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
             action()
+        }
+        .onReceive(NotificationCenter.default.publisher(for: NSNotification.Name.FXReachabilityStatusDidChange)) { _ in
+            if FXReachability.isReachable() {
+                action()
+            }
         }
     }
 }
