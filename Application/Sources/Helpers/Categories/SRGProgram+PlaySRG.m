03643b4257b5a15d2f277adb7013941497a5c694
Samuel Défago <defagos@gmail.com>
2020-10-23 17:39:55 +0200

    Avoid potential crashes due to incorrect data

--- Application/Sources/Helpers/Categories/SRGProgram+PlaySRG.m
+++ Application/Sources/Helpers/Categories/SRGProgram+PlaySRG.m
@@ -10,7 +10,11 @@
 
 - (BOOL)play_containsDate:(NSDate *)date
 {
-    NSDateInterval *dateInterval = [[NSDateInterval alloc] initWithStartDate:self.startDate endDate:self.endDate];
+    // Avoid potential crashes if data is incorrect
+    NSDate *startDate = [self.startDate earlierDate:self.endDate];
+    NSDate *endDate = [self.endDate laterDate:self.startDate];
+    
+    NSDateInterval *dateInterval = [[NSDateInterval alloc] initWithStartDate:startDate endDate:endDate];
     return [dateInterval containsDate:date];
 }
 
