04ed2a94150a664e8a079de8d622b948902a8a8c
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2020-11-05 16:11:18 +0100

    Use current service URL and global parameters for tvOS Letterbox controller

--- TV Application/Sources/Navigation.swift
+++ TV Application/Sources/Navigation.swift
@@ -16,6 +16,11 @@ func navigateToMedia(_ media: SRGMedia, play: Bool = false, animated: Bool = tru
     }
     else {
         let letterboxViewController = SRGLetterboxViewController()
+        
+        // TODO: Don't do it for AppStore build
+        letterboxViewController.controller.serviceURL = SRGDataProvider.current?.serviceURL
+        letterboxViewController.controller.globalParameters = SRGDataProvider.current?.globalParameters
+        
         letterboxViewController.controller.playMedia(media, at: nil, withPreferredSettings: nil)
         topViewController.present(letterboxViewController, animated: animated, completion: nil)
     }
