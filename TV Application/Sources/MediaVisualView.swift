e2c6ea9b996eed4260b001852e5e252413f26e0a
Samuel Défago <defagos@gmail.com>
2020-12-01 18:23:26 +0100

    Extract availability badge

--- TV Application/Sources/MediaVisualView.swift
+++ TV Application/Sources/MediaVisualView.swift
@@ -30,41 +30,6 @@ struct MediaVisualView: View {
         return YouthProtectionImageForColor(youthProtectionColor)
     }
     
-    static func formattedDuration(from: Date, to: Date) -> String? {
-        guard let days = Calendar.current.dateComponents([.day], from: from, to: to).day else { return nil }
-        switch days {
-        case 0:
-            return PlayShortFormattedHours(to.timeIntervalSince(from))
-        case 1...3:
-            return PlayShortFormattedDays(to.timeIntervalSince(from))
-        default:
-            return nil
-        }
-    }
-    
-    private func availabilityBadgeProperties() -> (text: String, color: Color)? {
-        guard let media = media else { return nil }
-        
-        let now = Date()
-        let availability = media.timeAvailability(at: now)
-        switch availability {
-        case .notYetAvailable:
-            return (NSLocalizedString("Soon", comment: "Short label identifying content which will be available soon."), Color(.play_gray))
-        case .notAvailableAnymore:
-            return (NSLocalizedString("Expired", comment: "Short label identifying content which has expired."), Color(.play_gray))
-        case .available:
-            guard let endDate = media.endDate, media.contentType != .livestream, media.contentType != .scheduledLivestream else { return nil }
-            if let remainingTime = Self.formattedDuration(from: now, to: endDate) {
-                return (String(format: NSLocalizedString("%@ left", comment: "Short label displayed on a media expiring soon"), remainingTime), Color(.play_orange))
-            }
-            else {
-                return nil
-            }
-        default:
-            return nil
-        }
-    }
-    
     private func updateProgress() {
         HistoryPlaybackProgressAsyncCancel(taskHandle)
         taskHandle = HistoryPlaybackProgressForMediaMetadataAsync(media, { progress = Double($0) })
@@ -96,16 +61,10 @@ struct MediaVisualView: View {
                     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
             }
             
-            Group {
-                if let isWebFirst = media?.play_isWebFirst, isWebFirst {
-                    Badge(text: NSLocalizedString("Web first", comment: "Web first label on media cells"), color: Color(.srg_blue))
-                }
-                else if let availabilityBadgeProperties = availabilityBadgeProperties() {
-                    Badge(text: availabilityBadgeProperties.text, color: availabilityBadgeProperties.color)
-                }
+            if let media = media {
+                AvailabilityBadge(media: media)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
             }
-            .padding([.leading, .top], 8)
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         }
         .onAppear {
             updateProgress()
