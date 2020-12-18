1c18acf9ac5078de771506686097b8c2caac94c1
Samuel Défago <defagos@gmail.com>
2020-12-18 11:57:49 +0100

    Update Content Protection to fix AirPlay issues with old Apple TVs
    … and update other libraries with minor fixes as well

--- Application/Sources/Helpers/PlayApplication.h
+++ Application/Sources/Helpers/PlayApplication.h
@@ -11,7 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  *  Function to run a block of code only once during the life of the application (under optional conditions)
  *
- *  @param ^block The block to be executed. The completion handler MUST be called after the code has been run. If
+ *  @param block  The block to be executed. The completion handler MUST be called after the code has been run. If
  *                the completion handler is called with success = YES, the block won't be run again
  *  @param key    The key to register block execution with
  *  @param object An optional object which must be associated with the key. If both do not match for some future
