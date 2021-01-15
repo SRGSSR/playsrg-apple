eb41205c73cf9c376e9b9bf747fff32439b89f86
Samuel Défago <defagos@gmail.com>
2021-01-15 11:21:41 +0100

    Implement basic search interface

--- TV Application/Sources/SearchViewController.swift
+++ TV Application/Sources/SearchViewController.swift
@@ -6,8 +6,43 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+// TODO: Turn into a UIHostingController
+// TODO: Somehow make searchController.searchControllerObservedScrollView = scrollView possible for SwiftUI views
+class SearchResultsViewController: UIViewController {
     override func loadView() {
         view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .white
+    }
+}
+
+class SearchViewController: UIViewController {
+    private let searchController: UISearchController
+    private let searchContainerViewController: UISearchContainerViewController
+    
+    init() {
+        searchController = UISearchController(searchResultsController: SearchResultsViewController())
+        searchContainerViewController = UISearchContainerViewController(searchController: searchController)
+        super.init(nibName: nil, bundle: nil)
+        
+        searchController.searchResultsUpdater = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Not implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addChild(searchContainerViewController)
+        searchContainerViewController.view.frame = view.bounds
+        view.addSubview(searchContainerViewController.view)
+        searchContainerViewController.didMove(toParent: self)
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
     }
 }
