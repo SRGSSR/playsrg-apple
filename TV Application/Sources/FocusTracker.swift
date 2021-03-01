824e39f215f5a63c54f6b700c43e41c6e1084417
Samuel Défago <defagos@gmail.com>
2021-03-01 19:17:48 +0100

    Fix SwiftUI internal crash
    Crash is related to the use of the background modifier (even with a trivial view). Rewrite modifier with a wrapper instead.

--- TV Application/Sources/FocusTracker.swift
+++ TV Application/Sources/FocusTracker.swift
@@ -6,25 +6,27 @@
 
 import SwiftUI
 
-fileprivate struct FocusTracker: View {
-    let action: (Bool) -> Void
+fileprivate struct FocusTracker<Content: View>: View {
+    private let action: (Bool) -> Void
+    private let content: () -> Content
     
     @Environment(\.isFocused) private var isFocused: Bool
     
+    init(action: @escaping (Bool) -> Void, @ViewBuilder content: @escaping () -> Content) {
+        self.action = action
+        self.content = content
+    }
+    
     var body: some View {
-        Rectangle()
-            .fill(Color.clear)
+        self.content()
             .onChange(of: isFocused) { action($0) }
     }
 }
 
 extension View {
-    /**
-     *  Apply modifier on a view wrapped in a focusable context. The provided action block will be called
-     *  each time a focus change is detected. This is useful to have parent or sibling views apply changes
-     *  due to one of their children getting the focus.
-     */
     func onFocusChange(perform action: @escaping (Bool) -> Void) -> some View {
-        self.background(FocusTracker(action: action))
+        FocusTracker(action: action) {
+            self
+        }
     }
 }
