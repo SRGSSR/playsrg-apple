8fb0c3e2fcbc04a840455f7c16cdd7ef5d590cc0
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2021-03-02 13:09:03 +0100

    Observe automatic playback continuation

--- TV Application/Sources/LetterboxDelegate.swift
+++ TV Application/Sources/LetterboxDelegate.swift
@@ -4,9 +4,33 @@
 //  License information is available from the LICENSE file.
 //
 
+import Combine
+
 class LetterboxDelegate: NSObject, SRGLetterboxViewControllerDelegate {
     static let shared = LetterboxDelegate()
     
+    var mainCancellables = Set<AnyCancellable>()
+
+    override init() {
+        NotificationCenter.default.publisher(for: Notification.Name.SRGLetterboxPlaybackDidContinueAutomatically, object: nil)
+            .sink { notification in
+                guard let media  = (notification.userInfo?[SRGLetterboxMediaKey]) as? SRGMedia else { return }
+
+                let labels = SRGAnalyticsHiddenEventLabels()
+                labels.source = AnalyticsSource.automatic.rawValue
+                labels.type = AnalyticsType.actionPlayMedia.rawValue
+                labels.value = media.urn
+
+                let controller = notification.object as! SRGLetterboxController
+                if controller.playlistDataSource is Playlist {
+                    let playlist = controller.playlistDataSource as! Playlist
+                    labels.extraValue1 = playlist.recommendationUid;
+                }
+                SRGAnalyticsTracker.shared.trackHiddenEvent(withName: AnalyticsTitle.continuousPlayback.rawValue, labels: labels)
+            }
+            .store(in: &mainCancellables)
+    }
+
     // MARK: - SRGLetterboxViewControllerDelegate protocol
 
     func letterboxViewController(_ letterboxViewController: SRGLetterboxViewController, didEngageInContinuousPlaybackWithUpcomingMedia upcomingMedia: SRGMedia) {
