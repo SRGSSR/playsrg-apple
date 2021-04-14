094e5fee0ec7f81d062938f28cca53f89d39cbe6
Samuel Défago <defagos@gmail.com>
2021-04-14 10:13:58 +0200

    Improve live media display

--- Application/Sources/UI/Views/DurationLabel.swift
+++ Application/Sources/UI/Views/DurationLabel.swift
@@ -9,29 +9,29 @@ import SwiftUI
 struct DurationLabel: View {
     let media: SRGMedia?
     
-    private var isLivestream: Bool {
-        guard let media = media else { return false }
-        return media.contentType == .livestream || media.contentType == .scheduledLivestream
-    }
-    
-    private var duration: String? {
+    private var properties: (text: String, color: Color)? {
         guard let media = media else { return nil }
-        if isLivestream {
-            return NSLocalizedString("Live", comment: "Short label identifying a livestream. Display in uppercase.")
+        if media.contentType == .livestream || media.contentType == .scheduledLivestream {
+            if media.blockingReason(at: Date()) == .startDate {
+                return (NSLocalizedString("Soon", comment: "Short label identifying content which will be available soon."), Color(white: 0, opacity: 0.5))
+            }
+            else {
+                return (NSLocalizedString("Live", comment: "Short label identifying a livestream."), Color(.play_red))
+            }
         }
         else {
-            return PlayShortFormattedMinutes(media.duration / 1000)
+            return (PlayShortFormattedMinutes(media.duration / 1000), Color(white: 0, opacity: 0.5))
         }
     }
     
     var body: some View {
-        if let duration = duration {
-            Text(duration)
+        if let properties = properties {
+            Text(properties.text)
                 .srgFont(.caption)
                 .foregroundColor(.white)
                 .padding(.vertical, 5)
                 .padding(.horizontal, 8)
-                .background(isLivestream ? Color(.play_liveRed) : Color(white: 0, opacity: 0.5))
+                .background(properties.color)
                 .cornerRadius(4)
         }
     }
