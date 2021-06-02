85a63fafe7ed72089286ef2970da538114794d52
Samuel Défago <defagos@gmail.com>
2021-06-02 16:18:41 +0200

    Adjust layout for all supported appearances

--- Application/Sources/Content/SectionShowHeaderView.swift
+++ Application/Sources/Content/SectionShowHeaderView.swift
@@ -40,20 +40,17 @@ struct SectionShowHeaderView: View {
         #endif
     }
     
-    private var spacing: CGFloat {
-        return (direction == .vertical) ? 20 : 0
-    }
-    
     var body: some View {
-        Stack(direction: direction, spacing: spacing) {
+        Stack(direction: direction, spacing: 0) {
             ImageView(url: show.imageUrl(for: .large))
                 .aspectRatio(SectionShowHeaderViewSize.aspectRatio, contentMode: .fit)
                 .background(Color.white.opacity(0.1))
-            VStack(spacing: 12) {
+            VStack(spacing: SectionShowHeaderViewSize.verticalSpacing) {
                 DescriptionView(section: section)
                 ShowAccessButton(show: show)
             }
             .padding(.horizontal, SectionShowHeaderViewSize.horizontalMargin)
+            .padding(.vertical)
             .frame(maxWidth: .infinity)
         }
         .padding(.bottom, SectionShowHeaderViewSize.verticalMargin)
@@ -64,15 +61,19 @@ struct SectionShowHeaderView: View {
         let section: Content.Section
         
         var body: some View {
-            VStack {
+            VStack(spacing: SectionShowHeaderViewSize.verticalSpacing) {
                 if let title = section.properties.title {
                     Text(title)
                         .srgFont(.H2)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.center)
                         .foregroundColor(.white)
                 }
                 if let summary = section.properties.summary {
                     Text(summary)
                         .srgFont(.body)
+                        .lineLimit(8)
+                        .multilineTextAlignment(.center)
                         .foregroundColor(.gray)
                 }
             }
@@ -82,6 +83,18 @@ struct SectionShowHeaderView: View {
     // Behavior: h-hug, v-hug
     private struct ShowAccessButton: View {
         let show: SRGShow
+        
+        #if os(iOS)
+        @Environment(\.horizontalSizeClass) var horizontalSizeClass
+        #endif
+        
+        var uiHorizontalSizeClass: UIUserInterfaceSizeClass {
+            #if os(iOS)
+            return UIUserInterfaceSizeClass(horizontalSizeClass)
+            #else
+            return .regular
+            #endif
+        }
         
         var body: some View {
             ResponderChain { firstResponder in
@@ -94,7 +107,7 @@ struct SectionShowHeaderView: View {
                     }
                     .padding(.horizontal, SectionShowHeaderViewSize.horizontalButtonPadding)
                     .padding(.vertical, SectionShowHeaderViewSize.verticalButtonPadding)
-                    .frame(maxWidth: .infinity, minHeight: 45, alignment: .leading)
+                    .adaptiveFrame(height: 45, for: uiHorizontalSizeClass)
                     .foregroundColor(.gray)
                     .background(Color.white.opacity(0.1))
                     .cornerRadius(LayoutStandardViewCornerRadius)
@@ -104,9 +117,30 @@ struct SectionShowHeaderView: View {
     }
 }
 
+// TODO: With Swift 5.5 use #if support for postfix expressions
+//       See https://github.com/apple/swift-evolution/blob/main/proposals/0308-postfix-if-config-expressions.md
+//
+//       That means:
+//         - Remove this extension
+//         - Remove uiHorizontalSizeClass
+//         - Directly inline it above with a separate expression per platform
+private extension View {
+    func adaptiveFrame(height: CGFloat, for horizontalSizeClass: UIUserInterfaceSizeClass? = .regular) -> some View {
+        return Group {
+            if horizontalSizeClass == .compact {
+                self.frame(maxWidth: .infinity, minHeight: height, alignment: .leading)
+            }
+            else {
+                self.frame(height: height)
+            }
+        }
+    }
+}
+
 class SectionShowHeaderViewSize: NSObject {
     fileprivate static let aspectRatio: CGFloat = 16 / 9
     
+    fileprivate static let verticalSpacing: CGFloat = constant(iOS: 18, tvOS: 24)
     fileprivate static let horizontalMargin: CGFloat = constant(iOS: 16, tvOS: 80)
     fileprivate static let verticalMargin: CGFloat = constant(iOS: 50, tvOS: 80)
     fileprivate static let horizontalButtonPadding: CGFloat = constant(iOS: 10, tvOS: 16)
@@ -131,10 +165,12 @@ struct SectionShowHeaderView_Previews: PreviewProvider {
             .previewLayout(.sizeThatFits)
         #else
         SectionShowHeaderView(section: .content(Mock.contentSection()), show: Mock.show())
+            .frame(width: 1000)
             .previewLayout(.sizeThatFits)
             .environment(\.horizontalSizeClass, .regular)
         
         SectionShowHeaderView(section: .content(Mock.contentSection()), show: Mock.show())
+            .frame(width: 375)
             .previewLayout(.sizeThatFits)
             .environment(\.horizontalSizeClass, .compact)
         #endif
