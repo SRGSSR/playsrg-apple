616d671abb3d136fda69efe139989f9bf383d641
Samuel Défago <defagos@gmail.com>
2021-07-09 11:01:32 +0200

    Restore legacy tvOS show A-Z view

--- TV Application/Sources/AppDelegate.swift
+++ TV Application/Sources/AppDelegate.swift
@@ -68,7 +68,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         if !configuration.areShowsUnavailable {
-            let showsViewController = SectionViewController(section: .configured(ConfiguredSection(type: .tvAllShows, contentPresentationType: .swimlane)))
+            let showsViewController = UIHostingController(rootView: ShowsView())
             showsViewController.tabBarItem = UITabBarItem(title: NSLocalizedString("Shows", comment: "Shows tab title"), image: nil, tag: 3)
             showsViewController.tabBarItem.accessibilityIdentifier = AccessibilityIdentifier.showsTabBarItem.rawValue
             viewControllers.append(showsViewController)
