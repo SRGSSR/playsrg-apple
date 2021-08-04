cc7b7703f2cc45d9b19011185dbcc7c24e06c020
Samuel Défago <defagos@gmail.com>
2021-08-04 11:45:14 +0200

    Prepare basic layout

--- Application/Sources/Program Guide/ProgramView.swift
+++ Application/Sources/Program Guide/ProgramView.swift
@@ -8,6 +8,7 @@ import SwiftUI
 
 // MARK: View
 
+// Behavior: h-exp, v-hug
 struct ProgramView: View {
     @Binding var program: SRGProgram
     @StateObject private var model = ProgramViewModel()
@@ -21,22 +22,60 @@ struct ProgramView: View {
     }
     
     var body: some View {
-        Text(program.title)
-            .padding()
-            .onAppear {
-                model.program = program
+        VStack {
+            ImageView(url: model.imageUrl)
+                .aspectRatio(16 / 9, contentMode: .fit)
+                .background(Color.white.opacity(0.1))
+                .cornerRadius(LayoutStandardViewCornerRadius)
+            DescriptionView(model: model)
+            Spacer()
+        }
+        .padding([.horizontal, .top], 14)
+        .onAppear {
+            model.program = program
+        }
+        .onChange(of: program) { newValue in
+            model.program = newValue
+        }
+    }
+    
+    // Behavior: h-exp, v-hug
+    private struct DescriptionView: View {
+        @ObservedObject var model: ProgramViewModel
+        
+        var body: some View {
+            VStack {
+                if let title = model.title {
+                    Text(title)
+                        .srgFont(.H2)
+                        .lineLimit(2)
+                        .foregroundColor(.srgGrayC7)
+                }
+                if let lead = model.lead {
+                    Text(lead)
+                        .srgFont(.H4)
+                        .lineLimit(2)
+                        .foregroundColor(.srgGray96)
+                }
+                if let summary = model.summary {
+                    Text(summary)
+                        .srgFont(.body)
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(.srgGray96)
+                }
             }
-            .onChange(of: program) { newValue in
-                model.program = newValue
-            }
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
 // MARK: Preview
 
 struct ProgramView_Previews: PreviewProvider {
+    private static let size = CGSize(width: 320, height: 600)
+    
     static var previews: some View {
         ProgramView(program: Mock.program())
-            .previewLayout(.sizeThatFits)
+            .previewLayout(.fixed(width: size.width, height: size.height))
     }
 }
--- Application/Sources/Program Guide/ProgramViewModel.swift
+++ Application/Sources/Program Guide/ProgramViewModel.swift
@@ -10,4 +10,20 @@ import Foundation
 
 final class ProgramViewModel: ObservableObject {
     @Published var program: SRGProgram?
+    
+    var title: String? {
+        return program?.title
+    }
+    
+    var lead: String? {
+        return program?.lead
+    }
+    
+    var summary: String? {
+        return program?.summary
+    }
+    
+    var imageUrl: URL? {
+        return program?.imageUrl(for: .medium)
+    }
 }
