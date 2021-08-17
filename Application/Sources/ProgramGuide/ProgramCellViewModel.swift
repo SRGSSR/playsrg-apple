758afe23d38500704af7207665b32a5364dd23f3
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2021-08-17 12:42:45 +0200

    Use accessibility translation

--- Application/Sources/ProgramGuide/ProgramCellViewModel.swift
+++ Application/Sources/ProgramGuide/ProgramCellViewModel.swift
@@ -25,7 +25,7 @@ final class ProgramCellViewModel: ObservableObject {
     
     var accessibilityLabel: String? {
         guard let program = program else { return nil }
-        return String(format: "From %1$@ to %2$@", PlayAccessibilityTimeFromDate(program.startDate), PlayAccessibilityTimeFromDate(program.endDate))
+        return String(format: PlaySRGAccessibilityLocalizedString("From %1$@ to %2$@", comment: "Text providing program time information. First placeholder is the start time, second is the end time."), PlayAccessibilityTimeFromDate(program.startDate), PlayAccessibilityTimeFromDate(program.endDate))
             .appending(", ")
             .appending(program.title)
     }
