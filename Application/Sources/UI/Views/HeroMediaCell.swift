30abd015f218c27a16cc3e05131b79263fa0a659
Samuel Défago <defagos@gmail.com>
2021-08-23 08:40:27 +0200

    Add hero cell layout

---
+++ Application/Sources/UI/Views/HeroMediaCell.swift
@@ -0,0 +1,71 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import SwiftUI
+
+// MARK: View
+
+struct HeroMediaCell: View {
+    let media: SRGMedia?
+    
+    var body: some View {
+        ZStack {
+            MediaVisualView(media: media, scale: .small)
+                .aspectRatio(HeroMediaCellSize.aspectRatio, contentMode: .fit)
+            DescriptionView(media: media)
+        }
+    }
+    
+    /// Behavior: h-exp, v-exp
+    private struct DescriptionView: View {
+        let media: SRGMedia?
+        
+        private var subtitle: String? {
+            guard let media = media else { return nil }
+            return MediaDescription.subtitle(for: media, style: .show)
+        }
+        
+        private var title: String? {
+            guard let media = media else { return nil }
+            return MediaDescription.title(for: media, style: .show)
+        }
+        
+        var body: some View {
+            VStack {
+                if let subtitle = subtitle {
+                    Text(subtitle)
+                        .srgFont(.subtitle1)
+                        .lineLimit(1)
+                }
+                if let title = title {
+                    Text(title)
+                        .srgFont(.H2)
+                        .lineLimit(2)
+                }
+            }
+            .multilineTextAlignment(.center)
+            .foregroundColor(.white)
+            .padding(.bottom, 50)
+            .padding(.horizontal, 18)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        }
+    }
+}
+
+// MARK: Size
+
+final class HeroMediaCellSize: NSObject {
+    fileprivate static let aspectRatio: CGFloat = 16 / 9
+}
+
+// MARK: Preview
+
+struct HeroMediaCell_Previews: PreviewProvider {
+    static var previews: some View {
+        HeroMediaCell(media: Mock.media())
+            .previewLayout(.fixed(width: 1000, height: 600))
+    }
+}
