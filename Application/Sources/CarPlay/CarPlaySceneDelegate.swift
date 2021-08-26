6cd4290af02f8c028e5ac83d26d358f00e45673f
Jordan Montel <jordan.montel@gmail.com>
2021-08-26 11:34:59 +0200

    Get RadioLiveStreams media and display on CarPlay

--- Application/Sources/CarPlay/CarPlaySceneDelegate.swift
+++ Application/Sources/CarPlay/CarPlaySceneDelegate.swift
@@ -1,29 +1,63 @@
 //
-//  CarPlaySceneDelegate.swift
-//  PlaySRG
+//  Copyright (c) SRG SSR. All rights reserved.
 //
-//  Created by Jordan Montel on 25/08/2021.
-//  Copyright © 2021 SRG SSR. All rights reserved.
+//  License information is available from the LICENSE file.
 //
 
+import Combine
 import CarPlay
 
 class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     
     var interfaceController: CPInterfaceController?
+    private var radioLiveStreamsViewModel = RadioLiveStreamsViewModel()
+    private var cancellables = Set<AnyCancellable>()
+    let radioLiveStreamsListTemplate: CPListTemplate = CPListTemplate(title: "Châines radio", sections: [])
+    let radioLiveStreamsGridTemplate: CPGridTemplate = CPGridTemplate(title: "Grid", gridButtons: [])
+
+    // MARK: - Custom Functions
+    func updateRadioLiveStreams(medias: [SRGMedia]) {
+        
+        var items: [CPListItem] = []
+
+        for media in medias {
+            print(media)
+            let radioChannel = ApplicationConfiguration.shared.radioChannel(forUid: media.channel?.uid)
+            let image = RadioChannelLogoImage(radioChannel)
+            let listItem = CPListItem(text: media.title, detailText: media.urn, image: image)
+            listItem.accessoryType = .disclosureIndicator
+            items.append(listItem)
+        }
+        let section = CPListSection(items: items)
+        radioLiveStreamsListTemplate.updateSections([section])
+    }
     
+    // MARK: - CPTemplateApplicationScene
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didConnect interfaceController: CPInterfaceController) {
         
         self.interfaceController = interfaceController
         
-        // Create a list
-        let item = CPListItem(text: "title", detailText: "detail")
-        item.accessoryType = .disclosureIndicator
-        let section = CPListSection(items: [item])
-        let listTemplate = CPListTemplate(title: "Section", sections: [section])
+        // Get radio live streams
+        radioLiveStreamsViewModel.$medias
+            .sink { [weak self] medias in
+                guard let strongSelf = self else { return }
+                strongSelf.updateRadioLiveStreams(medias: medias)
+            }
+            .store(in: &cancellables)
         
-        // Set root
-        self.interfaceController?.setRootTemplate(listTemplate, animated: true, completion: {_, _ in })
+        // Create a list
+//        let item = CPListItem(text: "title", detailText: "detail")
+//        item.accessoryType = .disclosureIndicator
+//        let section = CPListSection(items: [item])
+//        let listTemplate = CPListTemplate(title: "Section", sections: [section])
+//
+        
+        // Create a grid
+        
+        
+        // Create a tab bar
+        let tabBar = CPTabBarTemplate.init(templates: [radioLiveStreamsListTemplate, radioLiveStreamsGridTemplate])
+        self.interfaceController?.setRootTemplate(tabBar, animated: true, completion: {_, _ in })
     }
     
     // CarPlay disconnected
---
+++ Application/Sources/CarPlay/RadioLiveStreamsViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Foundation
+
+final class RadioLiveStreamsViewModel: ObservableObject {
+    @Published private(set) var medias: [SRGMedia] = []
+
+    init() {
+        SRGDataProvider.current!
+            .radioLivestreams(for: .RTS)
+            .replaceError(with: [])
+            .receive(on: DispatchQueue.main)
+            .assign(to: &$medias)
+    }
+}
