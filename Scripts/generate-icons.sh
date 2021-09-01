579d6e51da20da5e23808945ace65d3ca3061893
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2021-09-01 21:59:59 +0200

    Set a different caption color on app icon for App Center configurations

--- Scripts/generate-icons.sh
+++ Scripts/generate-icons.sh
@@ -115,8 +115,16 @@ do
                 CAPTION="${TITLE}"
             fi
 
+            if [ "${CONFIGURATION}" == "Nightly_AppCenter" ] || [ "${CONFIGURATION}" == "Beta_AppCenter" ]; then
+                BACKGROUND="#FFFC"
+                FILL="black"
+            else
+                BACKGROUND="#0005"
+                FILL="white"
+            fi
+
             echo $PYTHON_NIGHTLIES_TAG "Making app icon ${CAPTION} | ${filename}"
-            convert -background '#0005' -fill white -gravity center -size ${WIDTH}x${HEIGHT} caption:"${CAPTION}" "${SOURCE_ICON_PATH}" +swap -gravity south -composite "${SCRIPT_ICON_PATH}"
+            convert -background "${BACKGROUND}" -fill "${FILL}" -gravity center -size ${WIDTH}x${HEIGHT} caption:"${CAPTION}" "${SOURCE_ICON_PATH}" +swap -gravity south -composite "${SCRIPT_ICON_PATH}"
         fi
 
         SOURCE_ICON_PATH="${SCRIPT_ICON_PATH}"
