1ada3b503c69292c6bceb708bbc07d039f345ba7
Samuel Défago <defagos@gmail.com>
2021-09-15 09:12:35 +0200

    Extract transluscent background modifier

--- Application/Sources/UI/Views/Blur.swift
+++ Application/Sources/UI/Views/Blur.swift
@@ -13,7 +13,7 @@ import SwiftUI
 /**
  *  A view which blurs content behind it.
  */
-struct Blur: UIViewRepresentable {
+private struct Blur: UIViewRepresentable {
     let style: UIBlurEffect.Style
     
     func makeUIView(context: Context) -> UIVisualEffectView {
@@ -22,5 +22,21 @@ struct Blur: UIViewRepresentable {
     
     func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
         uiView.effect = UIBlurEffect(style: style)
+    }
+}
+
+// MARK: Extensions
+
+extension View {
+    /**
+     *  Apply a transluscent background.
+     */
+    func transluscentBackground() -> some View {
+#if os(iOS)
+        // TODO: When iOS 15 is the minimum supported version, replace with background(.thinMaterial)
+        return background(Blur(style: .systemThinMaterial))
+#else
+        return background(Color.clear)
+#endif
     }
 }
