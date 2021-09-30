9f29bc846292834a1c371d1cffa41e592f462a6f
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2021-09-30 11:08:03 +0200

    Improve method name

--- Application/Sources/Content/PageViewModel.swift
+++ Application/Sources/Content/PageViewModel.swift
@@ -314,7 +314,7 @@ private extension SRGDataProvider {
         #if os(tvOS)
         if rowItems.count > 0
             && (section.viewModelProperties.canOpenDetailPage || ApplicationSettingSectionWideSupportEnabled())
-            && section.viewModelProperties.hasSwimlaneLayout {
+            && section.viewModelProperties.hasMoreRowItem {
             rowItems.append(PageViewModel.Item(.more, in: section))
         }
         #endif
@@ -330,7 +330,8 @@ protocol PageViewModelProperties {
 }
 
 extension PageViewModelProperties {
-    var hasSwimlaneLayout: Bool {
+    #if os(tvOS)
+    var hasMoreRowItem: Bool {
         switch layout {
         case .mediaSwimlane, .showSwimlane, .highlightSwimlane:
             return true
@@ -338,6 +339,7 @@ extension PageViewModelProperties {
             return false
         }
     }
+    #endif
     
     var hasGridLayout: Bool {
         switch layout {
