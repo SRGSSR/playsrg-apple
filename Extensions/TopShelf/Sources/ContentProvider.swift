8a039aee7404c86cd8f79e94b742bfc1ce79b558
Samuel Défago <defagos@gmail.com>
2021-10-20 10:51:04 +0200

    Implement basic item retrieval

--- Extensions/TopShelf/Sources/ContentProvider.swift
+++ Extensions/TopShelf/Sources/ContentProvider.swift
@@ -4,10 +4,44 @@
 //  License information is available from the LICENSE file.
 //
 
+import SRGDataProviderCombine
 import TVServices
 
 final class ContentProvider: TVTopShelfContentProvider {
+    static let dataProvider: SRGDataProvider = {
+        return SRGDataProvider(serviceURL: SRGIntegrationLayerProductionServiceURL())
+    }()
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    private static func carouselItem(from media: SRGMedia) -> TVTopShelfCarouselItem {
+        let item = TVTopShelfCarouselItem(identifier: media.urn)
+        item.contextTitle = NSLocalizedString("Featured", comment: "Context title for items displayed in the tvOS top shelf")
+        item.title = media.title
+        item.summary = media.summary
+        item.duration = media.duration / 1000
+        return item
+    }
+    
+    private static func carouselContent(from medias: [SRGMedia]) -> TVTopShelfCarouselContent {
+        let items = medias.map { carouselItem(from: $0) }
+        return TVTopShelfCarouselContent(style: .details, items: items)
+    }
+    
+    private static func carouselContentPublisher() -> AnyPublisher<TVTopShelfContent?, Never> {
+        // FIXME: Use business unit associated with the application
+        return dataProvider.tvHeroStageMedias(for: .SRF)
+            .map { Optional(carouselContent(from: $0)) }
+            .replaceError(with: nil)
+            .eraseToAnyPublisher()
+    }
+    
     override func loadTopShelfContent(completionHandler: @escaping (TVTopShelfContent?) -> Void) {
-        completionHandler(nil)
+        Self.carouselContentPublisher()
+            .sink { content in
+                // Can be called from a background thread
+                completionHandler(content)
+            }
+            .store(in: &cancellables)
     }
 }
