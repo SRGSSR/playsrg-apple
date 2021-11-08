cac8b8edcce65ca8fa0a44a51c99014999d96016
Samuel Défago <defagos@gmail.com>
2021-11-08 08:02:53 +0100

    Display empty view when no program is available

--- Application/Sources/ProgramGuide/ProgramGuideGridViewController.swift
+++ Application/Sources/ProgramGuide/ProgramGuideGridViewController.swift
@@ -137,8 +137,8 @@ final class ProgramGuideGridViewController: UIViewController {
             emptyView.content = EmptyView(state: .loading)
         case let .failed(error: error):
             emptyView.content = EmptyView(state: .failed(error: error))
-        case .loaded:
-            emptyView.content = nil
+        case let .loaded(programCompositions):
+            emptyView.content = programCompositions.isEmpty ? EmptyView(state: .empty(type: .generic)) : nil
         }
         
         DispatchQueue.global(qos: .userInteractive).async {
