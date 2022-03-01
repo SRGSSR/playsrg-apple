efd560b7cc84cbec259ba517cdfba6c594f4f410
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2022-03-01 14:23:46 +0100

    Add youth protection accessibility information on media cell

--- Application/Sources/UI/Helpers/MediaDescription.swift
+++ Application/Sources/UI/Helpers/MediaDescription.swift
@@ -130,11 +130,19 @@ struct MediaDescription {
     }
     
     static func accessibilityLabel(for media: SRGMedia) -> String? {
+        let accessibilityLabel: String
         if let show = media.show, !areRedundant(media: media, show: show) {
-            return show.title.appending(", \(media.title)")
+            accessibilityLabel = show.title.appending(", \(media.title)")
         }
         else {
-            return media.title
+            accessibilityLabel = media.title
+        }
+        
+        if let youthProtectionLabel = SRGAccessibilityLabelForYouthProtectionColor(media.youthProtectionColor) {
+            return accessibilityLabel.appending(", \(youthProtectionLabel)")
+        }
+        else {
+            return accessibilityLabel
         }
     }
     
