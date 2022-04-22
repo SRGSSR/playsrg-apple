c68dc21ed81ab00d7a8cf482ad7970116f0b4d24
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2022-04-22 23:04:18 +0200

    Restore notification image

--- Application/Sources/Notifications/NotificationTableViewCell.m
+++ Application/Sources/Notifications/NotificationTableViewCell.m
@@ -14,6 +14,7 @@
 #import "UILabel+PlaySRG.h"
 
 @import SRGAppearance;
+@import SRGDataProvider;
 
 @interface NotificationTableViewCell ()
 
@@ -97,9 +98,8 @@
         imagePlaceholder = ImagePlaceholderMediaList;
     }
     
-    // FIXME:
-    NSAssert(NO, @"Must be fixed");
-    // [self.thumbnailImageView play_requestImageForObject:notification withScale:ImageScaleSmall type:SRGImageTypeDefault placeholder:imagePlaceholder];
+    SRGImage *image = [[SRGImage alloc] initWithURL:notification.imageURL variant:SRGImageVariantDefault];
+    [self.thumbnailImageView play_requestImage:image withSize:SRGImageSizeSmall placeholder:imagePlaceholder];
 }
 
 @end
