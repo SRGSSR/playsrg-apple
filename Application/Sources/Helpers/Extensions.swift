137755a67c3385d29fdd0ac026fa94ee85da03cc
Samuel Défago <defagos@gmail.com>
2022-05-04 11:18:35 +0200

    Transform computed property into constant

--- Application/Sources/Helpers/Extensions.swift
+++ Application/Sources/Helpers/Extensions.swift
@@ -56,8 +56,7 @@ extension String {
         return String(repeating: " ", count: length)
     }
     
-    static var loremIpsum: String {
-        return """
+    static let loremIpsum: String = """
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et \
             dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. \
             Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, \
@@ -83,7 +82,6 @@ extension String {
             sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero \
             eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.
             """
-    }
     
     func unobfuscated() -> String {
         return components(separatedBy: .decimalDigits).joined()
