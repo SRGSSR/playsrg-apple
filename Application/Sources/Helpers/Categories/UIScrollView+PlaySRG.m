93b020e2595438cae38c7964edc4af5082718ff5
Samuel Défago <defagos@gmail.com>
2022-06-14 09:24:07 +0200

    Fix TV guide top offset when scrolling to the top

--- Application/Sources/Helpers/Categories/UIScrollView+PlaySRG.m
+++ Application/Sources/Helpers/Categories/UIScrollView+PlaySRG.m
@@ -10,20 +10,41 @@
 
 @implementation UIScrollView (PlaySRG)
 
-+ (BOOL)canDisplayLargeTitleInViewController:(UIViewController *)viewController
+- (BOOL)play_canDisplayLargeTitle
 {
-    UINavigationBar *navigationBar = viewController.navigationController.navigationBar;
+    UIViewController *nearestViewController = self.play_nearestViewController;
+    UINavigationBar *navigationBar = nearestViewController.navigationController.navigationBar;
     return (navigationBar && ! navigationBar.hidden && navigationBar.prefersLargeTitles
-            && viewController.navigationItem.largeTitleDisplayMode != UINavigationItemLargeTitleDisplayModeNever);
+            && nearestViewController.navigationItem.largeTitleDisplayMode != UINavigationItemLargeTitleDisplayModeNever);
+}
+
+- (BOOL)play_isContainedInNavigationController
+{
+    UIViewController *nearestViewController = self.play_nearestViewController;
+    return nearestViewController.parentViewController == nearestViewController.navigationController;
+}
+
+- (CGFloat)play_navigationBarOffset
+{
+    if ([self play_canDisplayLargeTitle]) {
+        // 52 px is the additional height associated with large titles, see https://ivomynttinen.com/blog/ios-design-guidelines
+        if ([self play_isContainedInNavigationController]) {
+            return 52.f;
+        }
+        // To reveal a large navigation bar if it can be displayed, we have to scroll just a tiny bit before the top
+        // (1 px suffices) so that the large title is forced to be displayed.
+        else {
+            return 1.f;
+        }
+    }
+    else {
+        return 0.f;
+    }
 }
 
 - (void)play_scrollToTopAnimated:(BOOL)animated
 {
-    // To reveal a large navigation bar if it can be displayed, we have to scroll just a tiny bit before the top
-    // (1 px suffices) so that the large title is forced to be displayed.
-    UIViewController *nearestViewController = self.play_nearestViewController;
-    CGFloat navigationBarOffset = [UIScrollView canDisplayLargeTitleInViewController:nearestViewController] ? 1.f : 0.f;
-    [self setContentOffset:CGPointMake(self.contentOffset.x, -self.adjustedContentInset.top - navigationBarOffset) animated:animated];
+    [self setContentOffset:CGPointMake(self.contentOffset.x, -self.adjustedContentInset.top - [self play_navigationBarOffset]) animated:animated];
 }
 
 @end
