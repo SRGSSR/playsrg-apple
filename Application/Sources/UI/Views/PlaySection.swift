8d0751098076b88e885a14c9acd15ebcc0d67b9d
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2023-09-19 17:36:22 +0200

    Apply color on tvOS settings to fix tvOS 17 issue

--- Application/Sources/UI/Views/PlaySection.swift
+++ Application/Sources/UI/Views/PlaySection.swift
@@ -11,6 +11,11 @@ func PlaySection<Content: View, Header: View, Footer: View>(@ViewBuilder content
     return Section {
         content()
             .srgFont(.body)
+#if os(tvOS)
+        // tvOS 17.0 introduced a new issue when presenting modal, the default focused appearance is broken after modal presentation dismissal. See https://github.com/SRGSSR/playsrg-apple/issues/336
+            .foregroundColor(.white)
+            .listRowBackground(Color.srgGray33.cornerRadius(10))
+#endif
             .eraseToAnyView()
     } header: {
         header()
