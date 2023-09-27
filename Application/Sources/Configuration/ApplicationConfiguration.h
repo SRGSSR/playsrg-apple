7b7cd4fb5ee2ec1979e710335278d2fabe937466
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2023-09-27 21:16:36 +0200

    Fix property declaration

--- Application/Sources/Configuration/ApplicationConfiguration.h
+++ Application/Sources/Configuration/ApplicationConfiguration.h
@@ -97,7 +97,7 @@ OBJC_EXPORT NSString * const ApplicationConfigurationDidChangeNotification;
 
 @property (nonatomic, readonly, getter=isShowLeadPreferred) BOOL showLeadPreferred;
 
-@property (nonatomic, readonly, nullable) NSString *userConsentDefaultLanguage;
+@property (nonatomic, readonly, copy, nullable) NSString *userConsentDefaultLanguage;
 
 - (nullable RadioChannel *)radioChannelForUid:(nullable NSString *)uid;
 - (nullable RadioChannel *)radioHomepageChannelForUid:(nullable NSString *)uid;         // only returns a result if the radio channel exists and has a corresponding homepage
