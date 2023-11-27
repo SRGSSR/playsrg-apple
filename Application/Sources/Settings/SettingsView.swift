d743e1600f57961c5f05df7575b37f60b742b120
Pierre-Yves <py.bertholon@gmail.com>
2023-11-27 15:05:29 +0100

    Switch version button for tvOS TestFlight (#403)

--- Application/Sources/Settings/SettingsView.swift
+++ Application/Sources/Settings/SettingsView.swift
@@ -431,7 +431,7 @@ struct SettingsView: View {
                 }
 #if NIGHTLY || BETA
                 if let switchVersion = model.switchVersion {
-                    Button(NSLocalizedString("Switch version", comment: "Label of the button to open Apple TestFlight application and see other testable builds"), action: switchVersion)
+                    Button("\(NSLocalizedString("Switch version", comment: "Label of the button to open Apple TestFlight application and see other testable builds")) (TestFlight)", action: switchVersion)
                 }
 #else
                 if let becomeBetaTester = model.becomeBetaTester {
@@ -441,6 +441,10 @@ struct SettingsView: View {
                     Button(title, action: becomeBetaTester)
                 }
 #endif
+#else
+                if let switchVersion = model.switchVersion {
+                    Button("\(NSLocalizedString("Switch version", comment: "Label of the button to open Apple TestFlight application and see other testable builds")) (TestFlight)", action: switchVersion)
+                }
 #endif
                 VersionCell(model: model)
             } header: {
@@ -812,7 +816,7 @@ struct SettingsView: View {
         var body: some View {
             PlaySection {
                 if let switchVersion = model.switchVersion {
-                    Button(NSLocalizedString("Switch version", comment: "Label of the button to open Apple TestFlight application and see other testable builds"), action: switchVersion)
+                    Button("\(NSLocalizedString("Switch version", comment: "Label of the button to open Apple TestFlight application and see other testable builds")) (TestFlight)", action: switchVersion)
                 }
                 InformationSection.VersionCell(model: model)
             } header: {
