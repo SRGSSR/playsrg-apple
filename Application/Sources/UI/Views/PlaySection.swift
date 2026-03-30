d4c60bc28c8ef099ed2729b5cb540693c9186a11
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-03-30 15:41:32 +0200

    Avoid applying a style when not necessary on tvOS

--- Application/Sources/UI/Views/PlaySection.swift
+++ Application/Sources/UI/Views/PlaySection.swift
@@ -11,11 +11,9 @@ func PlaySection(@ViewBuilder content: () -> some View, @ViewBuilder header: () 
     Section {
         content()
             .srgFont(.body)
-        #if os(tvOS)
-            // tvOS 17.0 introduced a new issue when presenting modal, the default focused appearance is broken after modal presentation dismissal. See https://github.com/SRGSSR/playsrg-apple/issues/336
-            .foregroundColor(.white)
-            .listRowBackground(Color.srgGray33.cornerRadius(10))
-        #endif
+#if os(tvOS)
+            .tvOS17_backgroundFix()
+#endif
             .eraseToAnyView()
     } header: {
         header()
@@ -27,6 +25,21 @@ func PlaySection(@ViewBuilder content: () -> some View, @ViewBuilder header: () 
             .srgFont(.subtitle2)
             .foregroundColor(.srgGray96)
             .eraseToAnyView()
+    }
+}
+
+private extension View {
+    @available(iOS, unavailable)
+    @ViewBuilder
+    func tvOS17_backgroundFix() -> some View {
+        if ProcessInfo.processInfo.operatingSystemVersion.majorVersion == 17 {
+            // tvOS 17.0 introduced a new issue when presenting modal, the default focused appearance is broken after modal presentation dismissal. See https://github.com/SRGSSR/playsrg-apple/issues/336
+            foregroundColor(.white)
+                .listRowBackground(Color.srgGray33.cornerRadius(10))
+        }
+        else {
+            self
+        }
     }
 }
 
