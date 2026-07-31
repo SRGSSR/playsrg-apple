9070cbf01d4aa8be79a1f4c6d3d1cf9a5a69e338
Samuel Défago <defagos@gmail.com>
2026-07-31 13:45:52 +0200

    PLAYNEXT-8904 Avoid mixing global and receiver instances

--- Application/Sources/Helpers/Categories/UIApplication.swift
+++ Application/Sources/Helpers/Categories/UIApplication.swift
@@ -9,14 +9,14 @@ import Foundation
 extension UIApplication {
     var openTestFlight: (() -> Void)? {
         guard let appStoreAppleId = Bundle.main.object(forInfoDictionaryKey: "AppStoreAppleId") as? String, !appStoreAppleId.isEmpty else { return nil }
-        if let url = URL(string: "itms-beta://beta.itunes.apple.com/v1/app/\(appStoreAppleId)"), Self.shared.canOpenURL(url) {
+        if let url = URL(string: "itms-beta://beta.itunes.apple.com/v1/app/\(appStoreAppleId)"), canOpenURL(url) {
             return {
-                Self.shared.open(url)
+                self.open(url)
             }
-        } else if let url = URL(string: "https://beta.itunes.apple.com/v1/app/\(appStoreAppleId)"), Self.shared.canOpenURL(url) {
+        } else if let url = URL(string: "https://beta.itunes.apple.com/v1/app/\(appStoreAppleId)"), canOpenURL(url) {
             #if os(iOS)
                 return {
-                    Self.shared.open(url)
+                    self.open(url)
                 }
             #else
                 return nil
