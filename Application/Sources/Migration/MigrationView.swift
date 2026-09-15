8c1712cfc8e871fa83df5bb269b7d1a80898f9bc
Samuel Défago <defagos@gmail.com>
2026-09-15 11:21:36 +0200

    PLAYNEXT-9746 Make TV layout fit vertically

--- Application/Sources/Migration/MigrationView.swift
+++ Application/Sources/Migration/MigrationView.swift
@@ -108,31 +108,51 @@ struct MigrationView: View {
     let configuration: Configuration
 
     var body: some View {
-        GeometryReader { geometry in
-            ScrollView {
-                VStack(spacing: constant(iOS: 30, tvOS: 60)) {
-                    Spacer()
-                    descriptionView()
-                    Spacer()
-                    actionsView()
-                    footerView()
-                }
-                .padding(30)
-                .frame(maxWidth: geometry.size.width, minHeight: geometry.size.height)
-                .accessibilityAction(.escape) {
-                    presentationMode.wrappedValue.dismiss()
-                }
-            }
+        ZStack {
+            #if os(iOS)
+                mobileBody()
+            #else
+                tvBody()
+            #endif
         }
         .background(background())
     }
 
+    #if os(iOS)
+        private func mobileBody() -> some View {
+            GeometryReader { geometry in
+                ScrollView {
+                    VStack(spacing: 30) {
+                        Spacer()
+                        descriptionView()
+                        Spacer()
+                        actionsView()
+                        footerView()
+                    }
+                    .padding(30)
+                    .frame(maxWidth: geometry.size.width, minHeight: geometry.size.height)
+                    .accessibilityAction(.escape) {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }
+            }
+        }
+    #else
+        private func tvBody() -> some View {
+            VStack(spacing: 40) {
+                descriptionView()
+                Spacer()
+                actionsView()
+                footerView()
+            }
+            .padding(30)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    #endif
+
     private func descriptionView() -> some View {
         VStack(spacing: constant(iOS: 32, tvOS: 48)) {
             appIcon()
-            #if os(tvOS)
-                .focusable()
-            #endif
 
             Text(configuration.title)
                 .srgFont(.H1)
@@ -146,7 +166,7 @@ struct MigrationView: View {
                 bulletsView()
             }
         }
-        .frame(maxWidth: constant(iOS: 600, tvOS: 1024))
+        .frame(maxWidth: constant(iOS: 600, tvOS: 1200))
         .fixedSize(horizontal: false, vertical: true)
     }
 
@@ -197,36 +217,60 @@ struct MigrationView: View {
     }
 
     private func actionsView() -> some View {
-        VStack(spacing: 20) {
-            if let action = configuration.action {
-                Button {
-                    action()
-                    presentationMode.wrappedValue.dismiss()
-                } label: {
-                    Text(action.name)
-                    #if os(tvOS)
-                        .srgFont(.H3)
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical, 14)
-                    #endif
-                }
-                #if os(iOS)
-                .buttonStyle(.primary)
-                #endif
-            }
+        #if os(iOS)
+            mobileActionsView()
+        #else
+            tvActionsView()
+        #endif
+    }
 
-            if configuration.isCancellable {
-                Button("Cancel") {
-                    presentationMode.wrappedValue.dismiss()
+    #if os(iOS)
+        private func mobileActionsView() -> some View {
+            VStack(spacing: 20) {
+                if let action = configuration.action {
+                    Button {
+                        action()
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text(action.name)
+                    }
+                    .buttonStyle(.primary)
                 }
-                .srgFont(.H3)
-                .padding(.vertical, 14)
-                #if os(iOS)
+
+                if configuration.isCancellable {
+                    Button("Cancel") {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    .srgFont(.H3)
+                    .padding(.vertical, 14)
                     .foregroundColor(.white)
-                #endif
+                }
             }
         }
-    }
+    #else
+        private func tvActionsView() -> some View {
+            HStack(spacing: 40) {
+                if let action = configuration.action {
+                    Button {
+                        action()
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text(action.name)
+                            .srgFont(.H3)
+                    }
+                }
+
+                if configuration.isCancellable {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("Cancel")
+                            .srgFont(.H3)
+                    }
+                }
+            }
+        }
+    #endif
 
     private func background() -> some View {
         Image(.migrationBackground)
